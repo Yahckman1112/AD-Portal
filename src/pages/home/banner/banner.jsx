d86da78bad2a4f8876fdffeffa9728c01118184d
Yahckman <adeniranajibade2014@gmail.com>
2023-06-24 11:39:13 -0700

    skills

--- src/pages/home/banner/banner.jsx
+++ src/pages/home/banner/banner.jsx
@@ -1,14 +1,59 @@
 import React from "react";
 import styles from "./banner.module.scss";
 import { Link } from "react-router-dom";
+import { HiOutlineAcademicCap } from "react-icons/hi";
 function Banner(props) {
+  const skilsData = [
+    {
+      icon: <HiOutlineAcademicCap className={styles.skills_logo} />,
+      title: "Tajweed Class",
+      text: " Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    },
+    {
+      icon: <HiOutlineAcademicCap className={styles.skills_logo} />,
+      title: "Tajweed Class",
+      text: " Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    },
+    {
+      icon: <HiOutlineAcademicCap className={styles.skills_logo} />,
+      title: "Tajweed Class",
+      text: " Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    },
+    {
+      icon: <HiOutlineAcademicCap className={styles.skills_logo} />,
+      title: "Tajweed Class",
+      text: " Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    },
+  ];
   return (
-    <div className={styles.banner}>
-      <p className={styles.para1}>
-        The Best Center for Arabic and Islamic studies
-      </p>
-      <div>
-        <Link to='#' className={styles.btn}>Register Now</Link>
+    <div>
+      <div className={styles.banner}>
+        <p className={styles.para1}>
+          The Best Center for Arabic and Islamic studies
+        </p>
+        <div>
+          <Link to="#" className={styles.btn}>
+            Register Now
+          </Link>
+        </div>
+      </div>
+      <div className={styles.skills}>
+        <div className="container">
+          <div className="row">
+            {skilsData.map((item) => (
+              <div className="col-12 col-lg-3 col-md-12">
+                <div className={styles.skills_card}>
+                  <HiOutlineAcademicCap className={styles.skills_logo} />
+                  <p className={styles.skills_para1}>Tajweed Classes</p>
+                  <p className={styles.para2}>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Dolore, libero.
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
