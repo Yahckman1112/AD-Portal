de595ff45b4e9c7f45b4b7c653cc84c77c09ec54
Yahckman <adeniranajibade2014@gmail.com>
2023-06-24 10:54:34 -0700

    banner responsive

--- src/pages/home/banner/banner.jsx
+++ src/pages/home/banner/banner.jsx
@@ -5,7 +5,7 @@ function Banner(props) {
   return (
     <div className={styles.banner}>
       <p className={styles.para1}>
-        The Best Center for <br /> Arabic and Islamic studies
+        The Best Center for Arabic and Islamic studies
       </p>
       <div>
         <Link to='#' className={styles.btn}>Register Now</Link>
